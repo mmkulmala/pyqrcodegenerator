8c19f90fb78f9163fffacee77b24ca73367bd78c
Marno Kulmala <thejavasaurus@gmail.com>
2024-01-29 23:37:13 +0200

    Update make_qrcodes.py
    Adding comments and possibility to change font

--- make_qrcodes.py
+++ make_qrcodes.py
@@ -2,7 +2,10 @@ import segno
 import sys
 from PIL import Image, ImageDraw, ImageFont
 
-def create_qrcodes(filename):
+"""
+Create QR codes from a file with the specified filename and an optional font file name.
+"""
+def create_qrcodes(filename, fontFileName='AmericanTypewriter.ttc'):
     get_qrcode_amount(filename)
 
     with open(filename) as artist_file:
@@ -11,17 +14,26 @@ def create_qrcodes(filename):
             first_part_of_line = line.split(",")[0]
             name_of_file = first_part_of_line + '.png'
             link_to_launch = line.split(",")[1]
-            print(">> Generating: " +name_of_file + " that has data: " +link_to_launch)
+            print(">> Generating: " +name_of_file + " that goes to: " +link_to_launch)
             qrcode = segno.make_qr(link_to_launch)
             qrcode.save(
                 name_of_file,
-                scale=8,
+                scale=4,
                 border=5,
                 light=None,
             )
-            add_caption(name_of_file, first_part_of_line)
+            add_caption(name_of_file, first_part_of_line, fontFileName)
             n += 1
- 
+
+"""
+Calculate the number of qr-codes in the specified file.
+
+Args:
+    file_name (str): The name of the file containing the qr-codes.
+
+Returns:
+    None
+""" 
 def get_qrcode_amount(file_name):
     with open(file_name, 'r') as fp:
         x = len(fp.readlines())
@@ -29,20 +41,38 @@ def get_qrcode_amount(file_name):
         print('# Generating ' + str(x) + ' qr-codes                            #')   
         print('####################################################')
 
-def add_caption(caption_to, caption_what):
+"""
+Adds a caption to the specified image using the provided caption and font. 
+
+Args:
+    caption_to (str): The path to the image to which the caption should be added.
+    caption_what (str): The text of the caption.
+    fontFileName (str): The path to the font file to be used for the caption.
+
+Returns:
+    None
+"""
+def add_caption(caption_to, caption_what, fontFileName):
     image = Image.open(caption_to)
     width, height = image.size
     draw = ImageDraw.Draw(image)
     text_seat = caption_what
-    font = ImageFont.truetype('Chalkduster.ttf', 24)
-    draw.text((40,height - 40), text_seat, font=font)
+    font = ImageFont.truetype(fontFileName, 12)
+    draw.text((20,height - 20), text_seat, font=font)
     image.save(caption_to)   
 
+"""
+This function is the main entry point of the program. It checks the command line arguments
+and calls the create_qrcodes function with the appropriate parameters.
+"""
 def main():
-    if(len(sys.argv) != 2):
-        print("Usage: python3 make_qrcodes.py <info-text-file>")
+    if(len(sys.argv) < 2):
+        print("Usage: python3 make_qrcodes.py <artists-text-file> <optional-font-file-name>")
         sys.exit(1)
-    create_qrcodes(sys.argv[1])
+    if(len(sys.argv) == 2):
+        create_qrcodes(sys.argv[1])
+    if(len(sys.argv) == 3):
+        create_qrcodes(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
     main()
